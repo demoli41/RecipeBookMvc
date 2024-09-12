914915c56c41751bd8da4ff7fd576d18c6a99f43
SBA <SBA@DESKTOP-NR8TE9I>
2024-09-12 09:50:59 +0300

    Add project files.

---
+++ RecipeBookMvc/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeBookMvc.Models.DTO;
+using RecipeBookMvc.Repositories.Abstract;
+using System.Drawing.Text;
+
+namespace RecipeBookMvc.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private IUserAuthenticationService authService;
+        public UserAuthenticationController(IUserAuthenticationService authService)
+        {
+           this.authService = authService;
+        }
+        //public async Task<IActionResult> Register()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Email = "admin@gmail.com",
+        //        Username = "admin",
+        //        Name = "Bohdan",
+        //        Password = "Admin@123",
+        //        PasswordConfirm = "dmin@123",
+        //        Role = "Admin",
+        //    };
+        //    var result=await authService.RegisterAsync(model);
+        //    return Ok(result.Message);
+        //}
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await authService.LoginAsync(model);
+            if(result.StatusCode==1)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                TempData["msg"] = "Could not logged in";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+    }
+}
---
+++ RecipeBookMvc/Repositories/Implementation/UserAuthenticationService.cs
@@ -0,0 +1,146 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using RecipeBookMvc.Models.Domain;
+using RecipeBookMvc.Models.DTO;
+using RecipeBookMvc.Repositories.Abstract;
+using System.Security.Claims;
+
+namespace RecipeBookMvc.Repositories.Implementation
+{
+    public class UserAuthenticationService : IUserAuthenticationService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        public UserAuthenticationService(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.signInManager = signInManager;
+
+        }
+
+        public async Task<Status> RegisterAsync(RegistrationModel model)
+        {
+            var status = new Status();
+            var userExists = await userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+            {
+                status.StatusCode = 0;
+                status.Message = "Користувач уже існує";
+                return status;
+            }
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username,
+                Name = model.Name,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                status.StatusCode = 0;
+                status.Message = "Невдалось зареєструвати";
+                return status;
+            }
+
+            if (!await roleManager.RoleExistsAsync(model.Role))
+                await roleManager.CreateAsync(new IdentityRole(model.Role));
+
+
+            if (await roleManager.RoleExistsAsync(model.Role))
+            {
+                await userManager.AddToRoleAsync(user, model.Role);
+            }
+
+            status.StatusCode = 1;
+            status.Message = "Реєстрація успішна";
+            return status;
+        }
+
+
+        public async Task<Status> LoginAsync(LoginModel model)
+        {
+            var status = new Status();
+            var user = await userManager.FindByNameAsync(model.Username);
+            if (user == null)
+            {
+                status.StatusCode = 0;
+                status.Message = "Неправильне ім'я";
+                return status;
+            }
+
+            if (!await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                status.StatusCode = 0;
+                status.Message = "Неправильний пароль";
+                return status;
+            }
+
+            var signInResult = await signInManager.PasswordSignInAsync(user, model.Password, true, false);
+            if (signInResult.Succeeded)
+            {
+                var userRoles = await userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                };
+
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                status.StatusCode = 1;
+                status.Message = "Вхід успішний";
+            }
+            else if (signInResult.IsLockedOut)
+            {
+                status.StatusCode = 0;
+                status.Message = "Користувач вийшов";
+            }
+            else
+            {
+                status.StatusCode = 0;
+                status.Message = "Помилка входу";
+            }
+
+            return status;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await signInManager.SignOutAsync();
+
+        }
+
+        //public async Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username)
+        //{
+        //    var status = new Status();
+
+        //    var user = await userManager.FindByNameAsync(username);
+        //    if (user == null)
+        //    {
+        //        status.Message = "User does not exist";
+        //        status.StatusCode = 0;
+        //        return status;
+        //    }
+        //    var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+        //    if (result.Succeeded)
+        //    {
+        //        status.Message = "Password has updated successfully";
+        //        status.StatusCode = 1;
+        //    }
+        //    else
+        //    {
+        //        status.Message = "Some error occcured";
+        //        status.StatusCode = 0;
+        //    }
+        //    return status;
+
+        //}
+    }
+}
