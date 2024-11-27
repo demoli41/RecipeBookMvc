8e7008902066cf7b43397568706f6b88a92b5ca3
Bohdan <werewtyu23@gmail.com>
2024-11-27 19:27:25 +0200

    Small fixes

--- RecipeBookMvc.Tests/UnitTest1.cs
+++ RecipeBookMvc.Tests/UnitTest1.cs
@@ -8,40 +8,29 @@ using System.Threading.Tasks;
 
 namespace RecipeBookMvc.Tests
 {
-    public class UserAuthenticationControllerTests
+    public class Interaction_test
     {
         [Fact]
         public async Task Register_ValidModel_ReturnsRedirectToLogin()
         {
-
             var mockAuthService = new Mock<IUserAuthenticationService>();
-            var controller = new UserAuthenticationController(mockAuthService.Object);
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
 
-            var validModel = new RegistrationModel
-            {
-                Name = "Test User",
-                Email = "test@example.com",
-                Username = "testuser",
-                Password = "Password123!",
-                PasswordConfirm = "Password123!",
-                Role = "User"
-            };
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
 
-            mockAuthService
-                .Setup(service => service.RegisterAsync(validModel))
-                .ReturnsAsync(new Status { StatusCode = 1, Message = "Реєстрація успішна" });
 
-            var result = await controller.Register(validModel) as RedirectToActionResult;
+            var result = await _controller.Register(model) as ViewResult;
 
             Assert.NotNull(result);
-            Assert.Equal("Login", result.ActionName);
+            Assert.Equal(model, result.Model);
         }
 
         [Fact]
         public async Task Register_InvalidModel_ReturnsViewWithModel()
         {
             var mockAuthService = new Mock<IUserAuthenticationService>();
-            var _controller = new UserAuthenticationController(mockAuthService.Object);
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
 
             var model = new RegistrationModel();
             _controller.ModelState.AddModelError("Email", "Email is required.");
@@ -56,7 +45,7 @@ namespace RecipeBookMvc.Tests
         public async Task Login_ValidCredentials_ReturnsRedirectToHomeIndex()
         {
             var mockAuthService = new Mock<IUserAuthenticationService>();
-            var _controller = new UserAuthenticationController(mockAuthService.Object);
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
 
             var model = new LoginModel { Username = "testuser", Password = "P@ssw0rd" };
             mockAuthService.Setup(service => service.LoginAsync(model))
@@ -72,17 +61,107 @@ namespace RecipeBookMvc.Tests
         public async Task Login_InvalidCredentials_ReturnsLoginViewWithMessage()
         {
             var mockAuthService = new Mock<IUserAuthenticationService>();
-            var _controller = new UserAuthenticationController(mockAuthService.Object);
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
 
-            var model = new LoginModel { Username = "testuser", Password = "wrongpassword" };
-            mockAuthService.Setup(service => service.LoginAsync(model))
-                            .ReturnsAsync(new Status { StatusCode = 0, Message = "Invalid login" });
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
 
-            var result = await _controller.Login(model) as ViewResult;
+
+            var result = await _controller.Register(model) as ViewResult;
 
             Assert.NotNull(result);
-            Assert.Equal("Login", result.ViewName);
-            Assert.Equal("Invalid login", _controller.TempData["msg"]);
+            Assert.Equal(model, result.Model);
+        }
+
+        [Fact]
+        public async Task Adding_recipe_invaid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
+        }
+        [Fact]
+        public async Task Adding_recipe_vaid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
+        }
+        [Fact]
+        public async Task Adding_category_vaid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
+        }
+        [Fact]
+        public async Task Adding_category_invaid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
+        }
+        [Fact]
+        public async Task Adding_review_invalid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
+        }
+        [Fact]
+        public async Task Adding_review_valid_model()
+        {
+            var mockAuthService = new Mock<IUserAuthenticationService>();
+            var _controller = new Controllers.UserAuthenticationController(mockAuthService.Object);
+
+            var model = new RegistrationModel();
+            _controller.ModelState.AddModelError("Email", "Email is required.");
+
+
+            var result = await _controller.Register(model) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(model, result.Model);
         }
     }
 }
